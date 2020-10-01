771462c1d63cdff7baf4d080d6e4c0684efa9875
stigus96 <stighelgebruset@msn.com>
2020-10-01 13:40:55 +0200

    added createuser activity and added functions to multiple activities

---
+++ app/src/main/java/stighbvm/uials/no/rubikkannonsesystemapp/CreateUserActivity.java
@@ -0,0 +1,17 @@
+package stighbvm.uials.no.rubikkannonsesystemapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createuser);
+
+
+
+    }
+}
--- app/src/main/java/stighbvm/uials/no/rubikkannonsesystemapp/ListingActivity.java
+++ app/src/main/java/stighbvm/uials/no/rubikkannonsesystemapp/ListingActivity.java
@@ -2,13 +2,41 @@ package stighbvm.uials.no.rubikkannonsesystemapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ListingActivity extends AppCompatActivity {
+    private TextView createnewuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
+
+        createnewuser = (TextView) findViewById(R.id.createNewUser);
+        createnewuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateUserActivity();
+            }
+        });
+
+
+
+    }
+
+    public void openCreateUserActivity(){
+        Intent intent = new Intent(this, CreateUserActivity.class);
+        startActivity(intent);
     }
 }
